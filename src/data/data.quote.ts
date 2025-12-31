71baa20ade6c240a8045316ccedbf66166ff21c7
Zilvinas <zillsta@Mac.lan>
2025-12-31 12:03:51 +0200

    enable

--- src/data/data.quote.ts
+++ src/data/data.quote.ts
@@ -18,71 +18,71 @@ export const VALID_QUOTE_SCENARIOS = [
       fromAmount: "100000000000000000", // 0.001 ETH
       fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
     },
-  // },
-  // {
-  //   label: "CrossChainBridge", // Chain A to Chain B
-  //   params: {
-  //     fromChain: "ETH",
-  //     toChain: "POL",
-  //     fromToken: "ETH",
-  //     toToken: "USDC",
-  //     fromAmount: "200000000000000000", // 0.002 ETH
-  //     fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
-  //   },
-  // },
-  // {
-  //   label: "CrossChainBridge", // Chain A to Chain B
-  //   params: {
-  //     fromChain: 42161, // Arbitrum
-  //     toChain: 8453, // Base
-  //     fromToken: "USDC",
-  //     toToken: "ETH",
-  //     fromAmount: "20000000",
-  //     fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
-  //   },
-  // },
+  },
+  {
+    label: "CrossChainBridge", // Chain A to Chain B
+    params: {
+      fromChain: "ETH",
+      toChain: "POL",
+      fromToken: "ETH",
+      toToken: "USDC",
+      fromAmount: "200000000000000000", // 0.002 ETH
+      fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
+    },
+  },
+  {
+    label: "CrossChainBridge", // Chain A to Chain B
+    params: {
+      fromChain: 42161, // Arbitrum
+      toChain: 8453, // Base
+      fromToken: "USDC",
+      toToken: "ETH",
+      fromAmount: "20000000",
+      fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
+    },
+  },
 ];
 
 export const NEGATIVE_QUOTE_SCENARIOS: NegativeScenario[] = [
-  // {
-  //   label: "Missing Required Field (fromChain)",
-  //   params: {
-  //     toChain: 137,
-  //     fromToken: "ETH",
-  //     toToken: "USDC",
-  //     fromAmount: "1000000000000000000",
-  //     fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
-  //   },
-  //   expectedStatus: 400,
-  //   messagePart: "must have required property 'fromChain'",
-  // },
-  // {
-  //   label: "Zero Amount",
-  //   params: {
-  //     fromChain: 1,
-  //     toChain: 137,
-  //     fromToken: "ETH",
-  //     toToken: "USDC",
-  //     fromAmount: "0",
-  //     fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
-  //   },
-  //   expectedStatus: 400,
-  //   messagePart: '/fromamount must pass "isbignumberish" keyword validation', // Adjust based on actual API response
-  // },
-  // {
-  //   label: "Invalid Token Symbol",
-  //   params: {
-  //     fromChain: 1,
-  //     toChain: 137,
-  //     fromToken: "FAKE_TOKEN_123",
-  //     toToken: "USDC",
-  //     fromAmount: "1000000000000000000",
-  //     fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
-  //   },
-  //   expectedStatus: 400, // Or 404
-  //   messagePart:
-  //     "/fromtoken invalid address, /fromtoken unknown token symbol, /fromtoken must match exactly one schema in oneof",
-  // },
+  {
+    label: "Missing Required Field (fromChain)",
+    params: {
+      toChain: 137,
+      fromToken: "ETH",
+      toToken: "USDC",
+      fromAmount: "1000000000000000000",
+      fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
+    },
+    expectedStatus: 400,
+    messagePart: "must have required property 'fromChain'",
+  },
+  {
+    label: "Zero Amount",
+    params: {
+      fromChain: 1,
+      toChain: 137,
+      fromToken: "ETH",
+      toToken: "USDC",
+      fromAmount: "0",
+      fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
+    },
+    expectedStatus: 400,
+    messagePart: '/fromamount must pass "isbignumberish" keyword validation', // Adjust based on actual API response
+  },
+  {
+    label: "Invalid Token Symbol",
+    params: {
+      fromChain: 1,
+      toChain: 137,
+      fromToken: "FAKE_TOKEN_123",
+      toToken: "USDC",
+      fromAmount: "1000000000000000000",
+      fromAddress: "0x6DC1355e30A0A1114069002D6015a5D0d5992330",
+    },
+    expectedStatus: 400, // Or 404
+    messagePart:
+      "/fromtoken invalid address, /fromtoken unknown token symbol, /fromtoken must match exactly one schema in oneof",
+  },
   {
     label: "Negative Token amount",
     params: {
